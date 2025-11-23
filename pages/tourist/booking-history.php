5e49ead1fb64b1abd7f2bd90f50d646eb6f04590
unknown <alelybitun.school@gmail.com>
2025-11-23 22:36:14 +0800

    delete: delete Contents of booking-history.php

--- pages/tourist/booking-history.php
+++ pages/tourist/booking-history.php
@@ -3,29 +3,49 @@ session_start();
 if (!isset($_SESSION['user']) || $_SESSION['user']['role_name'] !== 'Tourist') {
     header('Location: ../../index.php');
     exit;
+} else if ($_SESSION['user']['account_status'] == 'Suspended') {
+    header('Location: account-suspension.php');
+    exit;
+} else if ($_SESSION['user']['account_status'] == 'Pending') {
+    header('Location: account-pending.php');
+    exit;
 }
-
-require_once "../../classes/booking.php";
 require_once "../../classes/tourist.php";
+require_once "../../classes/tour-manager.php";
+
+$TourManagerObj = new TourManager();
+$packages = $TourManagerObj->viewAllPackages();
+$packageCategory = $TourManagerObj->getTourSpotsCategory();
+
 
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Booking History/title>
-    <link rel="stylesheet" href="/../../assets/css/tourist/header.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking History</title>
+
     <link rel="stylesheet" href="../../assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../assets/vendor/components/font-awesome/css/all.min.css">
     <link rel="stylesheet" href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" >
-    
+    <link rel="stylesheet" href="../../assets/css/tourist/header.css">
 </head>
 <body>
-     <?php require_once "includes/header.php"; 
-    include_once "includes/header.php";?>
+    <?php require_once "includes/header.php";  ?>
+ 
 
-    <h2>My Bookings</h2>
+
+
+<main id="packagesContainer" class="main-contents ">
     
-    
+</main>
+
+
+<script>
+   
+</script>
+
+<script src="../../assets/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
